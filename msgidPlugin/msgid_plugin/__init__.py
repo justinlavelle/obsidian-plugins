78373d0969fb85b0c98f27b4a98e9fe7f42e2c2a
Fourat ZOUARI <fourat@ZF.local>
2017-12-04 20:07:47 +0100

    Fixing get_random_unique_msgid_plugin parameters

--- msgidPlugin/msgid_plugin/__init__.py
+++ msgidPlugin/msgid_plugin/__init__.py
@@ -1,6 +1,6 @@
 import uuid
 
 
-def get_random_unique_msgid_plugin(uid, destination_cid):
+def get_random_unique_msgid_plugin(pdu_type, uid, source_cid, destination_cid):
     # Return a random message uuid
     return str(uuid.uuid4())
